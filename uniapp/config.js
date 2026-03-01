92e77d36f64a4a6db58d915bae133fe98ae45af3
needasoul <lk1964825395@163.com>
2026-03-01 16:27:13 +0800

    update

--- uniapp/config.js
+++ uniapp/config.js
@@ -1,6 +1,6 @@
 export default {
 	// 服务的地址
-	serverIp: 'http://aa.ab/addons/boost/',
+	serverIp: 'https://zl.nankeyimeng.xyz/addons/boost/',
 	// 图片拼接地址
-	uploadImgUrl: 'http://aa.ab'
+	uploadImgUrl: 'https://zl.nankeyimeng.xyz'
 }
